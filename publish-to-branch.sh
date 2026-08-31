8941e3264a47dcba0753770a2b0e2888691e3ba6
SebastianMCarreira <sebastian.m.carreira@gmail.com>
2026-08-31 17:31:15 +0000

    Publish script

--- publish-to-branch.sh
+++ publish-to-branch.sh
@@ -6,7 +6,8 @@ npm run build
 $CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 git checkout $PUBLISH_BRANCH
 mv "$DIST_DIR/*" .
-git add *
+git add assets
+git add index.html
 git commit -m "Update published files"
 git push origin $PUBLISH_BRANCH
 git checkout $CURRENT_BRANCH
